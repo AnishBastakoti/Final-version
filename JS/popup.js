b95f4af42be4382561a7215e7e330d7df06354e8
AnishBastakoti <anishbastakoti7@gmail.com>
2020-05-15 21:55:48 +0930

    improvised

---
+++ JS/popup.js
@@ -0,0 +1,30 @@
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+
+function openCredit() {
+  document.getElementById("credit").style.display = "block";
+}
+
+function closeCredit() {
+  document.getElementById("credit").style.display = "none";
+}
